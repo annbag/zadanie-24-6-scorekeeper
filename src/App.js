43778838799ceaf961626066e9caccfad50bb19e
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-07-18 15:27:36 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -3,30 +3,41 @@ import './App.css';
 import PlayersList from './components/PlayersList/PlayersList';
 
 class App extends Component {
- constructor() {
-   super();
+  constructor() {
+    super();
+ 
+    this.state = {
+      players: [
+        {
+          name: 'Adam',
+          score: 5,
+        },
+        { 
+          name: 'Daniel',
+          score: 0,
+        }
+      ]
+    }    
+  }
 
-   this.state = {
-     players: [
-      {
-        name: 'Adam',
-        score: 5,
-      },
-      {
-        name: 'Daniel',
-        score: 0,
-      }
-    ]
-   }
- }
+  onScoreUpdate = (playerIndex, scoreChange) => {
+    this.setState({
+      players: this.state.players.map((player, index) => {
+        if (index === playerIndex) {
+          return { ...player, score: player.score + scoreChange };
+        }
+        return player;
+      })
+    })
+  }
 
- render() {
-   return (
-     <div className="App">
-       <PlayersList players={this.state.players} />
-     </div>
-   );
- }
+  render() {
+    return (
+      <div className="App">
+        <PlayersList players={this.state.players} onScoreUpdate={this.onScoreUpdate}/>
+      </div>
+    );
+  }
 }
 
 export default App;
