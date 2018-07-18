3edbdea01a38574d60a76d4aeee88468b632b3ca
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-07-18 14:59:05 +0200

    add style for PlayersList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import PlayersList from './components/PlayersList/PlayersList';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+ constructor() {
+   super();
+
+   this.state = {
+     players: [
+      {
+        name: 'Adam',
+        score: 5,
+      },
+      {
+        name: 'Daniel',
+        score: 0,
+      }
+    ]
+   }
+ }
+
+ render() {
+   return (
+     <div className="App">
+       <PlayersList players={this.state.players} />
+     </div>
+   );
+ }
 }
 
 export default App;
